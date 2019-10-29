0f227458010cc56d23c1dcf953577d9a62989bfa
odrodriguezc <odrodriguezc@gmail.com>
2019-10-29 23:00:24 +0100

    se incluye en esta version el ascenso de tin tin, la imagen del perro en la ventana, la animacion de las estrellas y la luna, una funcion alternativa para incrustar imagenes en lugar de iconos

--- js/main.js
+++ js/main.js
@@ -12,9 +12,6 @@ let compte = 10;
 let intervalID = null;
 let main;
 
-
-
-
 /***********************************************************************************/
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
@@ -31,11 +28,13 @@ function startCompte (){
 function compteRebour (){
     if (compte > 0){
         compte--;
-        dom.rocket.src = "images/rocket2.gif";
-    } else{
+        dom.rocket.src = "images/rocket2edit.gif";
+        dom.tintin1.classList.add('chao');
+    }else{
         stopCompte();
-        dom.rocket.src = "images/rocket3.gif";
+        dom.rocket.src = "images/rocket3edit.gif";
         dom.rocket.classList.toggle('tookOff');
+        createStars();
     }
     dom.billboard.innerText = compte;
 }
@@ -74,6 +73,30 @@ function createStars(){
     }
 }
 
+/*
+function createStars2(){
+    for (let i = 0; i <= 150; i++){
+        let  newStar = document.createElement('img');
+        newStar.src = "images/asteroid.svg";
+
+        newStar.classList.add('star');
+        switch (getRandomInteger(0,2)){
+            case 0:
+                newStar.classList.add('tiny');
+            break;
+            case 1:
+                newStar.classList.add('normal');
+            break;
+            default:
+                newStar.classList.add('big');
+        }
+    
+        starPosition(newStar);
+ 
+        let currentStar = document.querySelector('img star');
+        document.body.insertBefore(newStar, currentStar);   
+    }
+}*/
 
 function starPosition(newStar){
     let top =  getRandomInteger(0, 100) + "%";
@@ -87,7 +110,16 @@ function starPosition(newStar){
     newStar.style.right = right;
 }
 
+function welcome(){
+    dom.pBillboard.textContent = 'welcome to Apolo XXI';
+}
 
+function bording(){
+    dom.pBillboard.textContent = 'Preparing to board the rocket';
+    let elevator = document.querySelector('#elevator');
+    elevator.classList.add('boarding');
+    dom.tintin1.classList.add('walking');
+}
 
 /************************************************************************************/
 /* ******************************** CODE PRINCIPAL **********************************/
@@ -101,10 +133,16 @@ document.addEventListener('DOMContentLoaded', function(){
     dom.smoke = document.querySelector('#smoke');
     dom.smoke2 = document.querySelector('#smoke2');
     main = document.querySelector('main');
+    dom.pBillboard = document.querySelector('#billboard p');
+    dom.tintin1 = document.querySelector('#tintin1');
 
     dom.firingButton.addEventListener('click', startCompte);
     dom.cancelButton.addEventListener('click', abort);
 
-    createStars();
+    
+
+    welcome();
+    bording();
+
  
 });
